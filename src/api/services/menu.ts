655ec4c9f0fa7618a39d0f927e311d9019486f35
Fernando Moreira <feryamaha@hotmail.com>
2025-08-03 12:16:19 -0300

    pasta API deletada

--- src/api/services/menu.ts
+++
@@ -1,9 +0,0 @@
-import { api } from '../config/axios'
-import { MenuResponse } from '../types/menu'
-
-export const menuService = {
-  getBanners: async (): Promise<MenuResponse> => {
-    const response = await api.get('/menu-banners')
-    return response.data
-  }
-} 
